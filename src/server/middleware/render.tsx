250d4daad02f8842c64558c4b381dea743c314ff
Layne Faler <laynefaler@MacBook-Pro.lan>
2020-07-10 13:56:53 -0700

    Initial file structure

--- src/server/middleware/render.tsx
+++ src/server/middleware/render.tsx
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
-import App from '../../App';
+import App from '../../assets/components/App';
 
 const renderMiddleware = () => (req: Request, res: Response) => {
   let html = req.html || '';
