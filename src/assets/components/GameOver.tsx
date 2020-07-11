9e1a08c579460af07341ae845861f3848520bfe5
Layne Faler <laynefaler@MacBook-Pro.lan>
2020-07-11 14:22:39 -0700

    Added UI piece to game over

--- src/assets/components/GameOver.tsx
+++ src/assets/components/GameOver.tsx
@@ -12,8 +12,14 @@ const GameOver: React.FC<GameOverProps> = ({
 }) => {
   return (
     <div className="w-100 h-100 bg-dark d-flex flex-column align-items-center justify-content-center">
-      <div className="w-75 h-75 bg-light d-flex flex-column card shadow">
-
+      <div className="w-75 h-75 bg-light d-flex flex-column card align-items-between shadow">
+        <h1 className="text-center font-weight-light mt-5 mb-5">Game Over</h1>
+        {gameOverMessage && <h2 className="text-center font-weight-light mt-5 mb-5">{gameOverMessage}</h2>}
+        <div className="w-100 d-flex flex-row justify-content-around">
+          <button className="btn btn-outline-dark" onClick={() => playAgain()}>
+            Play Again
+          </button>
+        </div>
       </div>
     </div>
   );
