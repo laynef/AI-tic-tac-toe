85018db92077889b6184da7caa2cea102d1701fe
Layne Faler <laynefaler@MacBook-Pro.lan>
2020-07-12 16:29:09 -0700

    Added bootstrap styling

--- src/assets/components/LandingPage.tsx
+++ src/assets/components/LandingPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { DifficultyType } from '../tic_tac_toe/types';
+import logo from '../images/logo.svg';
 
 
 interface LandingPageProps {
@@ -9,13 +10,14 @@ interface LandingPageProps {
 const LandingPage: React.FC<LandingPageProps> = ({ startNewGame }) => {
   return (
     <div className="w-100 h-100 bg-dark d-flex flex-column align-items-center justify-content-center">
-      <div className="w-75 h-75 bg-light d-flex flex-column card align-items-between shadow">
+      <div className="w-75 h-75 bg-light d-flex flex-column card align-items-center justify-content-between shadow">
         <h1 className="text-center font-weight-light mt-5 mb-5">Tic Tac Toe</h1>
-        <div className="w-100 d-flex flex-row justify-content-around">
-          <button className="btn btn-outline-dark" onClick={() => startNewGame(DifficultyType.Beatable)}>
+        <img className="App-logo" src={logo} alt="" />
+        <div className="w-100 d-flex flex-row mt-5 mb-5 justify-content-around">
+          <button className="btn btn-outline-primary" onClick={() => startNewGame(DifficultyType.Beatable)}>
             Play Beatable
           </button>
-          <button className="btn btn-outline-dark" onClick={() => startNewGame(DifficultyType.Unbeatable)}>
+          <button className="btn btn-outline-danger" onClick={() => startNewGame(DifficultyType.Unbeatable)}>
             Play Impossible
           </button>
         </div>
