1facc1dc4792c8e45569a03c2f863e8e4b131cae
Layne Faler <laynefaler@MacBook-Pro.lan>
2020-07-13 15:46:10 -0700

    Removed service worker

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 
 import '../assets/styles/index.scss';
 import App from '../assets/components/Application';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.hydrate(<App />, document.getElementById('root'));
-serviceWorker.register();
